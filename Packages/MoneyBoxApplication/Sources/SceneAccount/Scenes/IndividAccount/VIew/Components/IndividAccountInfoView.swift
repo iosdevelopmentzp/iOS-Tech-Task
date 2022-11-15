25946cfe756484b919299220e919e11cae488aee
Dmytro Vorko <dmytro.vorko@cybg.com>
2022-11-15 15:51:25 +0000

    Ind Account info view draft

---
+++ Packages/MoneyBoxApplication/Sources/SceneAccount/Scenes/IndividAccount/VIew/Components/IndividAccountInfoView.swift
@@ -0,0 +1,8 @@
+//
+//  IndividAccountInfoView.swift
+//  
+//
+//  Created by Dmytro Vorko on 15/11/2022.
+//
+
+import Foundation
---
+++ Packages/MoneyBoxApplication/Sources/SceneAccount/Scenes/IndividAccount/VIew/Components/IndividAccountInfoViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  IndividAccountInfoViewModel.swift
+//  
+//
+//  Created by Dmytro Vorko on 15/11/2022.
+//
+
+import Foundation
+
+struct IndividAccountInfoViewModel {
+    
+}
